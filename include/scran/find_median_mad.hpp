514366914dc6f89f7b5721b37bed97c2f894c11b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-26 23:23:11 -0700

    Migrated code to find the median/MAD.

---
+++ include/scran/find_median_mad.hpp
@@ -0,0 +1,250 @@
+#ifndef SCRAN_FIND_MEDIAN_MAD_H
+#define SCRAN_FIND_MEDIAN_MAD_H
+
+#include <vector>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+#include <cstdint>
+
+#include "tatami_stats/tatami_stats.hpp"
+
+/**
+ * @file find_median_mad.hpp
+ * @brief Compute the median and MAD from an array of values.
+ */
+
+namespace scran {
+
+/**
+ * @namespace scran::find_median_mad
+ * @brief Calculate the median and MAD from an array of values.
+ *
+ * Pretty much as it says on the can; calculates the median of an array of values first,
+ * and uses the median to then compute the median absolute deviation (MAD) from that array.
+ * We support calculation of medians and MADs for separate "blocks" of observations in the same array.
+ */
+namespace find_median_mad {
+
+/**
+ * @brief Options for `find_median_mad::compute()`.
+ */
+struct Options {
+    /**
+     * Whether to compute the median and MAD after log-transformation of the values.
+     * This is useful for defining thresholds based on fold changes from the center.
+     * If `true`, all values are assumed to be non-negative.
+     */
+    bool log = false;
+
+    /**
+     * Whether to only compute the median.
+     * If true, `Results::mads` will be filled with NaNs.
+     */
+    bool median_only = false;
+};
+
+/**
+ * @tparam Value_ Floating-point type for the input.
+ * @tparam Index_ Integer type for array indices.
+ *
+ * @param num Number of observations.
+ * @param[in] metrics Pointer to an array of observations of length `n`.
+ * NaNs are ignored.
+ * Array contents are arbitrarily modified on function return and should not be used afterwards.
+ * @param options Further options.
+ *
+ * @return Pair containing the median and MAD for the `metrics`.
+ */
+template<typename Value_, typename Index_> 
+std::pair<Value_, Value_> compute(Index_ num, Value_* metrics, const Options& options) {
+    // Rotate all the NaNs to the front of the buffer and ignore them.
+    Index_ lost = 0;
+    for (Index_ i = 0; i < num; ++i) {
+        if (std::isnan(metrics[i])) {
+            std::swap(metrics[i], metrics[lost]);
+            ++lost;
+        }
+    }
+    metrics += lost;
+    num -= lost;
+
+    if (options.log) {
+        auto copy = metrics;
+        for (Index_ i = 0; i < num; ++i, ++copy) {
+            if (*copy > 0) {
+                *copy = std::log(*copy);
+            } else if (*copy == 0) {
+                *copy = -std::numeric_limits<double>::infinity();
+            } else {
+                throw std::runtime_error("cannot log-transform negative values");
+            }
+        }
+    }
+
+    // No need to skip the NaNs again.
+    auto median = tatami_stats::medians::direct(metrics, num, /* skip_nan = */ false);
+
+    if (options.median_only || std::isnan(median)) {
+        // Giving up.
+        return std::make_pair(median, std::numeric_limits<Value_>::quiet_NaN());
+    } else if (std::isinf(median)) {
+        // MADs should be no-ops when added/subtracted from infinity. Any
+        // finite value will do here, so might as well keep it simple.
+        return std::make_pair(median, static_cast<Value_>(0));
+    }
+
+    auto copy = metrics;
+    for (Index_ i = 0; i < num; ++i, ++copy) {
+        *copy = std::abs(*copy - median);
+    }
+    auto mad = tatami_stats::medians::direct(metrics, num, /* skip_nan = */ false);
+    mad *= 1.4826; // for equivalence with the standard deviation under normality.
+
+    return std::make_pair(median, mad);
+}
+
+/**
+ * @brief Temporary data structures for `compute_blocked()`.
+ *
+ * This can be re-used across multiple `compute_blocked()` calls to avoid reallocation.
+ *
+ * @tparam Value_ Floating-point type for the input.
+ * @tparam Index_ Integer type for array indices
+ */
+template<typename Value_, typename Index_>
+class Workspace {
+public:
+    /**
+     * @tparam Block_ Integer type for the block identifiers.
+     * @param num Number of observations.
+     * @param[in] block Pointer to an array of block identifiers, see `set()`.
+     */
+    template<typename Block_>
+    Workspace(Index_ num, const Block_* block) : my_buffer(num) {
+        set(num, block);
+    }
+
+    /**
+     * Default constructor.
+     */
+    Workspace() = default;
+
+    /**
+     * @tparam Block_ Integer type for the block identifiers.
+     * @param num Number of observations.
+     * @param[in] block Pointer to an array of block identifiers.
+     * The array should be of length equal to `num`.
+     * Values should be integer IDs in \f$[0, N)\f$ where \f$N\f$ is the number of blocks.
+     */
+    template<typename Block_>
+    void set(Index_ num, const Block_* block) {
+        my_block_starts.clear();
+
+        if (block) { 
+            for (Index_ i = 0; i < num; ++i) {
+                size_t candidate = block[i];
+                if (candidate >= my_block_starts.size()) {
+                    my_block_starts.resize(candidate + 1);
+                }
+                ++my_block_starts[candidate];
+            }
+
+            Index_ sofar = 0;
+            for (auto& s : my_block_starts) {
+                Index_ last = sofar;
+                sofar += s;
+                s = last;
+            }
+        }
+
+        my_buffer.resize(num);
+        my_block_ends.resize(my_block_starts.size());
+    }
+
+/**
+ * @cond
+ */
+public:
+    // Can't figure out how to make compute_blocked() a friend,
+    // so these puppies are public for simplicity.
+    std::vector<Value_> my_buffer;
+    std::vector<Index_> my_block_starts;
+    std::vector<Index_> my_block_ends;
+/**
+ * @endcond
+ */
+};
+
+/**
+ * @tparam Index_ Integer type for array indices
+ * @tparam Block_ Integer type, containing the block IDs.
+ * @tparam Value_ Floating-point type for the input.
+ *
+ * @param num Number of observations.
+ * @param[in] block Optional pointer to an array of block identifiers.
+ * If provided, the array should be of length equal to `num`.
+ * Values should be integer IDs in \f$[0, N)\f$ where \f$N\f$ is the number of blocks.
+ * If a null pointer is supplied, all observations are assumed to belong to the same block.
+ * @param[in] metrics Pointer to an array of observations of length `num`.
+ * @param workspace A workspace object, either (i) constructed on `num` and `block` or (ii) configured using `Workspace::set()` on `num` and `block`.
+ * The same object can be re-used across multiple calls to `compute_blocked()` with the same `num` and `block`.
+ * @param options Further options.
+ *
+ * @return Vector of length \f$N\f$, where each entry is a pair containing the median and MAD for each block in `block`.
+ */
+template<typename Index_, typename Block_, typename Value_>
+std::vector<std::pair<Value_, Value_> > compute_blocked(Index_ num, const Block_* block, const Value_* metrics, Workspace<Value_, Index_>& workspace, const Options& options) {
+    std::vector<std::pair<Value_, Value_> > output;
+
+    auto& buffer = workspace.my_buffer;
+    if (!block) {
+        std::copy_n(metrics, num, buffer.begin());
+        output.push_back(compute(num, buffer.data(), options));
+        return output;
+    }
+
+    const auto& starts = workspace.my_block_starts;
+    auto& ends = workspace.my_block_ends;
+    std::copy(starts.begin(), starts.end(), ends.begin());
+    for (Index_ i = 0; i < num; ++i) {
+        auto& pos = ends[block[i]];
+        buffer[pos] = metrics[i];
+        ++pos;
+    }
+
+    // Using the ranges on the buffer.
+    size_t nblocks = starts.size();
+    output.reserve(nblocks);
+    for (size_t g = 0; g < nblocks; ++g) {
+        output.push_back(compute(ends[g] - starts[g], buffer.data() + starts[g], options));
+    }
+
+    return output;
+}
+
+/**
+ * Overload that handles the creation of the `Workspace`.
+ *
+ * @tparam Index_ Integer type for array indices
+ * @tparam Block_ Integer type, containing the block IDs.
+ * @tparam Value_ Floating-point type for the input.
+ *
+ * @param num Number of observations.
+ * @param[in] block Pointer to an array of block identifiers, see `compute_blocked()`.
+ * @param[in] metrics Pointer to an array of observations of length `num`.
+ * @param options Further options.
+ *
+ * @return Vector of length \f$N\$f, where each entry is a pair containing the median and MAD for each block in `block`.
+ */
+template<typename Index_, typename Block_, typename Value_>
+std::vector<std::pair<Value_, Value_> > compute_blocked(Index_ num, const Block_* block, const Value_* metrics, const Options& options) {
+    Workspace<Value_, Index_> wrk(num, block);
+    return compute_blocked(num, block, metrics, wrk, options);
+}
+
+}
+
+}
+
+#endif
---
+++ tests/src/find_median_mad.cpp
@@ -0,0 +1,182 @@
+#include <gtest/gtest.h>
+
+#include "find_median_mad.hpp"
+
+static std::vector<double> even_values = { 
+    0.761164335, 0.347582428, 0.430822695, 0.888530395, 0.627701241, 0.678912751,
+    0.097218816, 0.692552865, 0.143479605, 0.049907948, 0.648174966, 0.848563147,
+    0.472604294, 0.022525487, 0.738983761, 0.915699533, 0.577269375, 0.799325422,
+    0.554883985, 0.009624974, 0.215816610
+};
+
+TEST(FindMedianMad, BasicTests) {
+    auto copy = even_values;
+
+    scran::find_median_mad::Options opt;
+    auto stats = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+    EXPECT_FLOAT_EQ(stats.first, 0.5772693750);
+    EXPECT_FLOAT_EQ(stats.second, 0.3292202953);
+
+    // Checking for odd.
+    copy = even_values;
+    auto stats2 = scran::find_median_mad::compute(copy.size() - 1, copy.data(), opt);
+    EXPECT_FLOAT_EQ(stats2.first, 0.6024853080);
+    EXPECT_FLOAT_EQ(stats2.second, 0.2731710715);
+}
+
+TEST(FindMedianMad, EdgeTests) {
+    scran::find_median_mad::Options opt;
+
+    std::vector<double> empty;
+    auto stats = scran::find_median_mad::compute(empty.size(), empty.data(), opt);
+    EXPECT_TRUE(std::isnan(stats.first));
+    EXPECT_TRUE(std::isnan(stats.second));
+
+    empty.push_back(std::numeric_limits<double>::infinity());
+    stats = scran::find_median_mad::compute(empty.size(), empty.data(), opt);
+    EXPECT_TRUE(std::isinf(stats.first));
+    EXPECT_TRUE(stats.first > 0);
+    EXPECT_EQ(stats.second, 0);
+    
+    empty.push_back(-std::numeric_limits<double>::infinity());
+    stats = scran::find_median_mad::compute(empty.size(), empty.data(), opt);
+    EXPECT_TRUE(std::isnan(stats.first));
+    EXPECT_TRUE(std::isnan(stats.second));
+
+    empty.push_back(-std::numeric_limits<double>::infinity()); // add twice to keep it even.
+    empty.push_back(-std::numeric_limits<double>::infinity());
+    stats = scran::find_median_mad::compute(empty.size(), empty.data(), opt);
+    EXPECT_TRUE(std::isinf(stats.first));
+    EXPECT_TRUE(stats.first < 0);
+    EXPECT_EQ(stats.second, 0);
+}
+
+TEST(FindMedianMad, MissingFilter) {
+    scran::find_median_mad::Options opt;
+
+    // One at the end.
+    {
+        auto copy = even_values;
+        auto ref = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+
+        copy = even_values;
+        copy.push_back(std::numeric_limits<double>::quiet_NaN());
+        auto withnan = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+        EXPECT_EQ(ref, withnan);
+    }
+
+    // Two at the start.
+    {
+        auto copy = even_values;
+        copy[0] = std::numeric_limits<double>::quiet_NaN();
+        copy[1] = std::numeric_limits<double>::quiet_NaN();
+        auto withnan = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+
+        copy = even_values;
+        auto ref = scran::find_median_mad::compute(copy.size() - 2, copy.data() + 2, opt);
+        EXPECT_EQ(ref, withnan);
+    }
+}
+
+TEST(FindMedianMad, MedianOnly) {
+    scran::find_median_mad::Options opt;
+    opt.median_only = true;
+
+    auto copy = even_values;
+    auto stats = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+
+    EXPECT_FLOAT_EQ(stats.first, 0.5772693750);
+    EXPECT_TRUE(std::isnan(stats.second));
+}
+
+TEST(FindMedianMad, LogTests) {
+    scran::find_median_mad::Options opt;
+    opt.log = true;
+
+    auto copy = even_values;
+    auto isres = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+    EXPECT_FLOAT_EQ(isres.first, -0.5494462670);
+    EXPECT_FLOAT_EQ(isres.second, 0.4825257172);
+
+    // Handles occasional zero values.
+    {
+        auto copy1 = even_values;
+        copy1.push_back(0.0000001);
+        auto copy2 = even_values;
+        copy2.push_back(0);
+
+        auto isres1 = scran::find_median_mad::compute(copy1.size(), copy1.data(), opt);
+        auto isres2 = scran::find_median_mad::compute(copy2.size(), copy2.data(), opt);
+        EXPECT_EQ(isres1, isres2);
+    }
+
+    // Does something sensible with loads of zeroes.
+    {
+        std::vector<double> empty(even_values.size());
+        auto isres = scran::find_median_mad::compute(empty.size(), empty.data(), opt);
+        EXPECT_TRUE(std::isinf(isres.first));
+        EXPECT_TRUE(isres.first < 0);
+        EXPECT_EQ(isres.second, 0);
+    }
+}
+
+TEST(FindMedianMad, BlockTests) {
+    std::vector<int> block {
+        0, 1, 2, 3, 
+        1, 0, 2, 3,
+        1, 2, 0, 3,
+        1, 2, 3, 0,
+        0, 0, 0, 0, 
+        1
+    };
+
+    scran::find_median_mad::Options opt;
+    auto isres = scran::find_median_mad::compute_blocked(even_values.size(), block.data(), even_values.data(), opt);
+    EXPECT_EQ(isres.size(), 4);
+
+    for (int i = 0; i < 4; ++i) {
+        std::vector<double> copy;
+        for (size_t j = 0; j < block.size(); ++j) {
+            if (block[j] == i) {
+                copy.push_back(even_values[j]);
+            }
+        }
+
+        auto is2res = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+        EXPECT_EQ(isres[i], is2res);
+    }
+
+    // NULL blocked falls back to single-batch processing.
+    {
+        auto copy = even_values;
+        auto ref = scran::find_median_mad::compute(copy.size(), copy.data(), opt);
+        auto isres_none = scran::find_median_mad::compute_blocked(even_values.size(), static_cast<int*>(NULL), even_values.data(), opt);
+        EXPECT_EQ(isres_none.size(), 1);
+        EXPECT_EQ(ref, isres_none[0]);
+    }
+}
+
+TEST(FindMedianMad, WorkspaceReuse) {
+    std::vector<int> block { 
+        0, 1, 1, 2,
+        2, 1, 1, 1,
+        2, 2, 1, 1,
+        2, 2, 0, 0,
+        0, 0, 2, 0,
+        2
+    };
+
+    scran::find_median_mad::Options opt;
+    scran::find_median_mad::Workspace<double, int> work(block.size(), block.data());
+    auto isres = scran::find_median_mad::compute_blocked<int>(even_values.size(), block.data(), even_values.data(), work, opt);
+    auto isres2 = scran::find_median_mad::compute_blocked<int>(even_values.size(), block.data(), even_values.data(), work, opt);
+    EXPECT_EQ(isres, isres2);
+
+    // Now trying to reuse the same workspace but with different blocking.
+    std::reverse(block.begin(), block.end());
+    auto ref = scran::find_median_mad::compute_blocked(even_values.size(), block.data(), even_values.data(), opt);
+    work.set(block.size(), block.data());
+    isres = scran::find_median_mad::compute_blocked<int>(even_values.size(), block.data(), even_values.data(), work, opt);
+    isres2 = scran::find_median_mad::compute_blocked<int>(even_values.size(), block.data(), even_values.data(), work, opt);
+    EXPECT_EQ(isres, isres2);
+}
