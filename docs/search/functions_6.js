9b83ef662e196fc8bbe4092cd0641b711b06a34f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-28 17:13:06 +0000

    Deploying to gh-pages from @ libscran/quality_control@cb2c2f7c499bbedd170434c8f076bc42c3c5d82e 🚀

--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['get_0',['get',['https://tatami-inc.github.io/tatami/classtatami_1_1ConsecutiveOracle.html#a7310f20500d941eb27eb367b3f930b88',1,'tatami::ConsecutiveOracle::get()'],['https://tatami-inc.github.io/tatami/classtatami_1_1FixedVectorOracle.html#ac3a568555ec15ccbed4aab42d5dddb37',1,'tatami::FixedVectorOracle::get()'],['https://tatami-inc.github.io/tatami/classtatami_1_1FixedViewOracle.html#a6ff53180a31ec5c4a0f3d7f7eb026e0f',1,'tatami::FixedViewOracle::get()'],['https://tatami-inc.github.io/tatami/classtatami_1_1Oracle.html#a852153ae7d04d12fdea97ebb7b11f357',1,'tatami::Oracle::get()']]],
-  ['get_5flower_1',['get_lower',['../classscran_1_1choose__filter__thresholds_1_1Thresholds.html#a951a531ee7d08dac4a27105ea6f1dc4a',1,'scran::choose_filter_thresholds::Thresholds::get_lower()'],['../classscran_1_1choose__filter__thresholds_1_1BlockThresholds.html#ae23ef5a8ef97f87d4b9b5dec0ef4e5d0',1,'scran::choose_filter_thresholds::BlockThresholds::get_lower()']]],
-  ['get_5fupper_2',['get_upper',['../classscran_1_1choose__filter__thresholds_1_1Thresholds.html#a48e7480877d17b404a02e5245e2ddc49',1,'scran::choose_filter_thresholds::Thresholds::get_upper()'],['../classscran_1_1choose__filter__thresholds_1_1BlockThresholds.html#ab195c7a040d4e3794360730ba484286d',1,'scran::choose_filter_thresholds::BlockThresholds::get_upper()']]]
+  ['get_5flower_1',['get_lower',['../classscran_1_1choose__filter__thresholds_1_1Thresholds.html#a951a531ee7d08dac4a27105ea6f1dc4a',1,'scran::choose_filter_thresholds::Thresholds']]],
+  ['get_5fthresholds_2',['get_thresholds',['../classscran_1_1choose__filter__thresholds_1_1BlockThresholds.html#a46cb24c8bf43c4edbe7378b1dae89fb0',1,'scran::choose_filter_thresholds::BlockThresholds']]],
+  ['get_5fupper_3',['get_upper',['../classscran_1_1choose__filter__thresholds_1_1Thresholds.html#a48e7480877d17b404a02e5245e2ddc49',1,'scran::choose_filter_thresholds::Thresholds']]]
 ];
