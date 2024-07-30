63b88f55ed4f046597e1fcb6107b6d64290ebc94
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-07-30 17:47:40 +0000

    Deploying to gh-pages from @ libscran/scran_qc@6315218b279b1808e8aa9d2246857a1a39dce126 🚀

--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['reference_0',['reference',['https://tatami-inc.github.io/tatami/structtatami_1_1SomeNumericArray_1_1Iterator.html#a4bf2843ca912061c3cc0400bbb75abe6',1,'tatami::SomeNumericArray::Iterator']]],
-  ['retrieve_5fcompressed_5fsparse_5fcontents_1',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#ad723a47ee2e03e4213cd07fda8710e65',1,'tatami']]],
+  ['retrieve_5fcompressed_5fsparse_5fcontents_1',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a6b95eab5d7370f965e4d6a9b9a9f3393',1,'tatami']]],
   ['retrieve_5ffragmented_5fsparse_5fcontents_2',['retrieve_fragmented_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#ab5688af2360372f1535d6d68b2c14de6',1,'tatami']]],
   ['rna_5fquality_5fcontrol_2ehpp_3',['rna_quality_control.hpp',['../rna__quality__control_8hpp.html',1,'']]],
   ['rnaqcblockedfilters_4',['RnaQcBlockedFilters',['../classscran__qc_1_1RnaQcBlockedFilters.html',1,'scran_qc']]],
