82f8358a4ec08156d996a36389568b08a81c2a30
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-02-15 22:34:27 +0000

    Deploying to gh-pages from @ libscran/scran_qc@4d63c930495a580c5b2c447411c7d532508011f5 🚀

--- docs/search/classes_4.js
+++ docs/search/classes_4.js
@@ -8,6 +8,7 @@ var searchData=
   ['fragmentedsparsecolumnmatrix_5',['FragmentedSparseColumnMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseColumnMatrix.html',1,'tatami']]],
   ['fragmentedsparsecontents_6',['FragmentedSparseContents',['https://tatami-inc.github.io/tatami/structtatami_1_1FragmentedSparseContents.html',1,'tatami']]],
   ['fragmentedsparsematrix_7',['FragmentedSparseMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseMatrix.html',1,'tatami']]],
-  ['fragmentedsparserowmatrix_8',['FragmentedSparseRowMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseRowMatrix.html',1,'tatami']]],
-  ['fullsparsifiedwrapper_9',['FullSparsifiedWrapper',['https://tatami-inc.github.io/tatami/classtatami_1_1FullSparsifiedWrapper.html',1,'tatami']]]
+  ['fragmentedsparsematrix_3c_20value_5f_2c_20index_5f_2c_20valuevectorstorage_5f_2c_20indexvectorstorage_5f_20_3e_8',['FragmentedSparseMatrix&lt; Value_, Index_, ValueVectorStorage_, IndexVectorStorage_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseMatrix.html',1,'tatami']]],
+  ['fragmentedsparserowmatrix_9',['FragmentedSparseRowMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseRowMatrix.html',1,'tatami']]],
+  ['fullsparsifiedwrapper_10',['FullSparsifiedWrapper',['https://tatami-inc.github.io/tatami/classtatami_1_1FullSparsifiedWrapper.html',1,'tatami']]]
 ];
